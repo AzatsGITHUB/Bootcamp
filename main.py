8f5d0d2a3d2174322c690f05f12f1890e3214986
Alisher Argynbekov <alisher.argynbekov@gmail.com>
2023-02-26 19:46:27 +0600

    modify function to Upperize or Lowerize

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-def upperize(string: str):
-    return string.upper()
-    # TODO: Huyina predelai
+import os
+
+
+def up_or_lower(string: str, flag=0) -> str:
+    """
+    Upperize or lowerize a string depending on flag.
+
+    :param string:
+    :param flag:
+    :return:
+    """
+    if flag:
+        return string.upper()
+    else:
+        return string.lower()
+    # TODO: Azat aga, accept eteniz, otinem.
 
 
 if __name__ == '__main__':
-    print(upperize("hello world!"))
+    case = int(os.environ.get("CASE"))
+    print(up_or_lower("Hello world!", case))
